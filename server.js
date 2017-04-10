34ce4c0756e6ee422b5e3974381e9cd93bbab947
Deven Panchal <devenrpanchal@gmail.com>
2017-04-10 17:12:15 +0000

    Define routing router

--- server.js
+++ server.js
@@ -4,75 +4,8 @@ var app = express();
 app.set('port', (process.env.PORT || 8081));
 app.set('view engine', 'ejs');
 
-// This responds with "Hello World" on the homepage
-app.get('/', function (req, res) {
-   console.log("Got a GET request for the homepage");
-   res.send('Hello GET');
-})
-
-// This responds a POST request for the homepage
-app.post('/', function (req, res) {
-   console.log("Got a POST request for the homepage");
-   res.send('Hello POST');
-})
-
-// This responds a DELETE request for the /del_user page.
-app.delete('/del_user', function (req, res) {
-   console.log("Got a DELETE request for /del_user");
-   res.send('Hello DELETE');
-})
-
-// This responds a GET request for the /list_user page.
-app.get('/list_user', function (req, res) {
-   console.log("Got a GET request for /list_user");
-   res.send('Page Listing');
-})
-
-// This responds a GET request for abcd, abxcd, ab123cd, and so on
-app.get('/ab*cd', function(req, res) {   
-   console.log("Got a GET request for /ab*cd");
-   res.send('Page Pattern Match');
-})
-
-//Send static file index.htm when queries for it specifically
-app.use(express.static('public'));
-
-app.get('/index.htm', function (req, res) {
-   res.sendFile( __dirname + "/" + "index.htm" );
-})
-
-
-app.get('/process_get', function (req, res) {
-   // Prepare output in JSON format
-   response = {
-      first_name:req.query.first_name,
-      last_name:req.query.last_name
-   };
-   console.log(response);
-   res.end(JSON.stringify(response));
-})
-
-app.post('/file_upload', function (req, res) {
-   console.log(req.files.file.name);
-   console.log(req.files.file.path);
-   console.log(req.files.file.type);
-   var file = __dirname + "/" + req.files.file.name;
-   
-   fs.readFile( req.files.file.path, function (err, data) {
-      fs.writeFile(file, data, function (err) {
-         if( err ){
-            console.log( err );
-            }else{
-               response = {
-                  message:'File uploaded successfully',
-                  filename:req.files.file.name
-               };
-            }
-         console.log( response );
-         res.end( JSON.stringify( response ) );
-      });
-   });
-})
+// Requests on / handles by the routing router
+app.use('/', routing);
 
 var server = app.listen(app.get('port'), function () {
 
