1b63675e1d4cb69e097f4d6b7b73e84e4eeb103d
Ubuntu <ubuntu@ip-172-31-29-72.us-west-2.compute.internal>
2017-04-10 19:29:37 +0000

    Add middlewares

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 var express = require('express');
 var app = express();
 
+//Mount body-parser to parse URL encoded data and json data
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Mount cookie parser
+var cookieParser = require('cookie-parser');
+app.use(cookieParser())
+
 app.set('port', (process.env.PORT || 8081));
+
 app.set('view engine', 'ejs');
 
-// require router module
+// Require router module
 var myrouter = require('./routing.js');
+
 // Requests on / handles by the routing router
 app.use('/', myrouter);
+
+// request date logger middleware
+app.use(function(req, res, next){
+	console.log("A new request received at " + Date.now());
+	next();
+});
+
+
+// App level error handling middleware.
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('This is the App-level error handling middleware. Something broke!')
+})
 
 var server = app.listen(app.get('port'), function () {
 
